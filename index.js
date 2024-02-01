c47f181d5b0761936c0f16f106df66651bb6ce56
Okolosiokaro <okolosiokaro@gmail.com>
2024-01-31 16:59:53 -0800

    js

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
+const profile = document.querySelector('#profile');
+
+
 function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
+    
 }
+
+window.onload = function() {
+const homePic = document.querySelector('#home-pic');
+homePic.classList.add('animate__animated', 'animate__fadeInLeft');
+
+const homeText = document.querySelector('#home-text');
+homeText.classList.add('animate__animated', 'animate__fadeInRight');
+};
+
+
